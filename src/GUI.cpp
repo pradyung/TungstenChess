66aaee418ddd4faca63e8c99cf2475fdef150d0f
pradyung <103707604+pradyung@users.noreply.github.com>
2024-01-02 15:19:30 -0600

    moved entry point to GUI.cpp

--- src/GUI.cpp
+++ src/GUI.cpp
@@ -1,5 +1,18 @@
 #include "Headers/GUI.hpp"
 
+using namespace Chess;
+
+int main()
+{
+  RenderWindow window(VideoMode(SQUARE_SIZE * 8, SQUARE_SIZE * 8), "Chess", sf::Style::Titlebar | sf::Style::Close);
+
+  GUIHandler gui(window);
+
+  gui.runMainLoop();
+
+  return 0;
+}
+
 namespace Chess
 {
   GUIHandler::GUIHandler(RenderWindow &window)
