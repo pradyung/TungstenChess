66113dbd41d4fc891e81d0975cbb22a447aa4f42
pradyung <103707604+pradyung@users.noreply.github.com>
2025-02-22 15:58:30 -0600

    Moved entry point to main.cpp

--- src/GUI.cpp
+++ src/GUI.cpp
@@ -1,14 +1,5 @@
 #include "GUI.hpp"
 
-int main()
-{
-  RenderWindow window(VideoMode(SQUARE_SIZE * 8, SQUARE_SIZE * 8), "TungstenChess", sf::Style::Titlebar | sf::Style::Close);
-
-  TungstenChess::GUIHandler gui(window);
-
-  gui.runMainLoop();
-}
-
 namespace TungstenChess
 {
   GUIHandler::GUIHandler(RenderWindow &window)
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include "GUI.hpp"
+
+int main()
+{
+  RenderWindow window(VideoMode(SQUARE_SIZE * 8, SQUARE_SIZE * 8), "TungstenChess", sf::Style::Titlebar | sf::Style::Close);
+
+  TungstenChess::GUIHandler gui(window);
+
+  gui.runMainLoop();
+}
